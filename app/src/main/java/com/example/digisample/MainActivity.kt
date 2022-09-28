2476380b84263f847165517de75216962c7ad921
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-28 22:45:09 +0300

    custom loading class added

--- app/src/main/java/com/example/digisample/MainActivity.kt
+++ app/src/main/java/com/example/digisample/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.digisample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.custom_loading_view.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
