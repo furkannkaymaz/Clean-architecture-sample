cf4847ddb9ea5070c94bd15874097c4f42f52ba2
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-11-16 17:37:50 +0300

    Update SongViewModelTest.kt

--- app/src/test/java/com/example/cleanarcsample/presentation/songs/ui/SongViewModelTest.kt
+++ app/src/test/java/com/example/cleanarcsample/presentation/songs/ui/SongViewModelTest.kt
@@ -20,6 +20,7 @@ class SongViewModelTest {
     private lateinit var viewModel: SongViewModel
     private lateinit var userCase: GetSongUserCase
 
+
     @Before
     fun setUp() {
         userCase = GetSongUserCase(FakeSongRepository(getDummyData()))
@@ -35,27 +36,27 @@ class SongViewModelTest {
 
     @Test
     fun `get song list  limit invalid exceeded return assertFalse`() = runBlocking {
-        val data = viewModel.getSongs("asdasd", offset = 8, limit = 0)
+        val data = viewModel.getSongs("Şarkı Listesi", offset = 8, limit = 0)
         delay(600)
         assertFalse(data.value.data != null)
     }
 
     @Test
     fun `get song list keyword limit exceeded return false`() = runBlocking {
-        val data = viewModel.getSongs("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 2, 2)
+        val data = viewModel.getSongs("Çok uzun bir yazı deneme testi", 2, 2)
         delay(600)
         assertFalse(data.value.data != null)
     }
     @Test
     fun `get song list offset limit exceeded return false`() = runBlocking {
-        val data = viewModel.getSongs("AA", 455, 2)
+        val data = viewModel.getSongs("Aa", 455, 2)
         delay(600)
         assertFalse(data.value.data != null)
 
     }
     @Test
     fun `get song list keyword under 3 letter return empty list`() = runBlocking {
-        val data = viewModel.getSongs("AA", 45, 2)
+        val data = viewModel.getSongs("A", 45, 2)
         delay(600)
         assertEquals(data.value.data?.results, arrayListOf<SongModelResult>())
 
